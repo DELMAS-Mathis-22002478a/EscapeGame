0ce3de6506c1cf735d751febaca3674dce632e56
MatisD <119317520+MatisD@users.noreply.github.com>
2023-02-02 23:59:49 +0100

    Add files via upload

--- siteEscape/jvstyle.js
+++ siteEscape/jvstyle.js
@@ -1,19 +1,27 @@
 let button = document.querySelector('#button');
 let msg = document.querySelector('#welcome');
+let msgwelcome = document.querySelector('#welcomemessage');
 let body = document.querySelector('body');
 let img = document.querySelector('#im');
 let button2 = document.querySelector('#button2');
 let wh = document.querySelector('#welcomehide');
+let play = document.querySelector('#wrapperPlay');
+let search = document.querySelector('#searchBar');
+
 
 
 
 button.addEventListener('click', ()=>{
-  body.classList.toggle('back');
+ 
   msg.classList.toggle('welcomeback');
+  msgwelcome.classList.toggle('welcomeback');
   wh.classList.toggle('reveal');
+  button.classList.toggle('back');
+  search.classList.toggle('reveal');
 
   
 })
+
 
 function change(){
   let displayImage = document.getElementById('im');
@@ -21,8 +29,20 @@ function change(){
     displayImage.src='ampouleV.png';
   } else{
     displayImage.src='ampoule.png';
-    
   }
+  
 }
 
+function jouer(){
+  var a = document.getElementById('searchBar').value;
+  if(a === "jouer" || a === "JOUER"){
+    play.classList.toggle('reveal');
+  }
+}
+play.addEventListener('click', ()=>{
+ 
+  window.open('./niveau1.html')
+  window.open('http://google.com')
+  
+})
 
