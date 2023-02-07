e4d786f8adc2acca531d475195e9fc003a45c845
MatisD <119317520+MatisD@users.noreply.github.com>
2023-02-07 10:21:33 +0100

    Add files via upload

--- siteEscape/jvstyle.js
+++ siteEscape/jvstyle.js
@@ -7,6 +7,7 @@ let button2 = document.querySelector('#button2');
 let wh = document.querySelector('#welcomehide');
 let play = document.querySelector('#wrapperPlay');
 let search = document.querySelector('#searchBar');
+let modalbtn = document.querySelector('#myBtn')
 
 
 
@@ -18,7 +19,8 @@ button.addEventListener('click', ()=>{
   wh.classList.toggle('reveal');
   button.classList.toggle('back');
   search.classList.toggle('reveal');
-
+  modalbtn.classList.toggle('reveal');
+  body.classList.toggle('background');
   
 })
 
@@ -42,7 +44,30 @@ function jouer(){
 play.addEventListener('click', ()=>{
  
   window.open('./niveau1.html')
-  window.open('http://google.com')
+ 
   
 })
 
+// Référence au bouton et à la fenêtre modale
+var btn = document.getElementById("myBtn");
+var modal = document.getElementById("myModal");
+
+// Référence à la croix de fermeture
+var span = document.getElementsByClassName("close")[0];
+
+// Afficher la fenêtre modale lorsque le bouton est cliqué
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Fermer la fenêtre modale lorsque la croix est cliquée
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Fermer la fenêtre modale lorsque l'utilisateur clique en dehors de la fenêtre
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
